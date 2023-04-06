9231204ff7de2e7d26200069dd3ede29cf40bdc7
Liudmila Skripkina <msk.revizor@gmail.com>
2023-04-06 10:28:23 +0300

    Added js-code for slider-arrows

--- js/main.js
+++ js/main.js
@@ -10,3 +10,27 @@ menuIcon.addEventListener('click', () => {
    // запрещаем скролл при открытом меню
    // body.classList.toggle('no-scroll');
 });
+
+//* SLIDER-ARROWS
+const sliderArrows = document.querySelector(".slider-arrows");
+const slidesArrows = sliderArrows.querySelectorAll(".slider-arrows__item");
+const prev = sliderArrows.querySelector(".slider-arrows__arrow_prev");
+const next = sliderArrows.querySelector(".slider-arrows__arrow_next");
+
+// Текущий слайд
+let slideIndex = 0;
+
+prev.addEventListener('click', () => showSlideArrows(-1));
+next.addEventListener('click', () => showSlideArrows(1));
+
+function showSlideArrows(n = 0) {
+   slideIndex += n;
+
+   if (slideIndex < 0) slideIndex = slidesArrows.length - 1;
+   if (slideIndex >= slidesArrows.length) slideIndex = 0;
+
+   slidesArrows.forEach(item => item.style.display = "none");
+   slidesArrows[slideIndex].style.display = "block";
+}
+
+showSlideArrows(0);
